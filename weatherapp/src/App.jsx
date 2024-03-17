a14565909dae013cc096292e3c1cb943c5231c54
Asif Ahmed <mohammed.a.ahmed@se22.qmul.ac.uk>
2024-03-17 13:59:55 +0000

    Update package.json, package-lock.json, App.css, App.jsx, and index.css.

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Weather from './Weather';
+import WindMap from './windmap';
 const App = () => { return (
 <div>
-  <Weather />
+  {/* <Weather /> */}
+  <WindMap />
+
 </div> );
 };
 export default App;
---
+++ weatherapp/src/windmap.jsx
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import styles from './windmap.css';
+import NavBar from './nav-bar';
+import DaysOfWeek from './daysOfWeek';
+import homeIcon from './Assets/home.png';
+import location from './Assets/location.png';
+import burgerIcon from './Assets/burgericon.png'
+import windIcon from './Assets/Wind.png'
+
+const WindMap = (props) => {
+    
+    return(
+        
+        <div className="background">
+            
+            <div className="info-container">
+                <div className="info">
+                    <p>wind map</p>
+                    
+                    <DaysOfWeek/>
+
+                    <div className="wind-info">
+                       
+                        <img id="wind1" src={windIcon} alt='Wind Icon'/>
+                        <p id="wind-speed">Wind: 392</p>
+                        <img id="wind2" src={windIcon} alt='Wind Icon'/>
+                        <p id="gust-speed">Gust: 900</p>
+                    </div>
+                </div>
+            </div>
+            
+            < NavBar/>
+
+            
+
+        </div>
+    )
+
+};
+
+export default WindMap;
