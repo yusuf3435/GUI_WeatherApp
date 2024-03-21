5715e2ea5f2e34db99b025b7033ca70ac864ed4a
ayat224 <163903707+ayat224@users.noreply.github.com>
2024-03-21 01:03:09 +0000

    Update App.jsx

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,43 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Weather from './Weather';
-import WindMap from './windmap';
-import { Search } from './search';
-import homeIcon from './Assets/home.png';
-import location from './Assets/location.png';
-import burgerIcon from './Assets/burgericon.png'
-
-const App = () => {
-  const [currentScreen, setCurrentScreen] = useState('search');
-
-  const switchToSearch = () => {
-    setCurrentScreen('search');
-  };
-
-  const switchToWindMap = () => {
-    setCurrentScreen('windmap');
-  };
-
-  return (
-    <div>
-      <div className='nav-bar'>
-      <button onClick={switchToSearch} className='button' >
-      <img className="location" src={location} alt="location pin"/>
-      </button>
-      <button onClick={switchToWindMap}   className='button'>
-      <img className="burger" src={burgerIcon} alt="burger icon"/>
-      </button>
-      <button className='button'> 
-        <img className="home" src={homeIcon} alt="home icon"/>
-      </button>
-      </div>
-
-
-      {currentScreen === 'search' && <Search />}
-      {currentScreen === 'windmap' && <WindMap />}
-    </div>
-  );
+import Weekly from './weekly.js';
+const App = () => { return (
+<div>
+  {/* <Weather /> */}
+  <Weekly />
+</div> );
 };
-
 export default App;
-
 
